1bb15c11ce9c78848d7b670302a159a1e825d1a2
IsaacIngram22 <iingram@students.casciahall.com>
2019-10-08 18:54:56 -0500

    Organized into new files after reseting all programs and added new teleopmain

---
+++ MainTeleop/TeleopMain.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.MainTeleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+public class TeleopMain {
+
+    // todo: write your code here
+}
