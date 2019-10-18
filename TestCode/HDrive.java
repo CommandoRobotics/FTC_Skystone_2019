f666b597f1822b11d3ca943fde4993c8c0c44792
IsaacIngram22 <iingram@students.casciahall.com>
2019-10-18 10:39:18 -0500

    Added DcMotor declarations into driveForwardToPosition method to fix E-Stop

--- TestCode/HDrive.java
+++ TestCode/HDrive.java
@@ -10,19 +10,22 @@ public class HDrive {
     DcMotor rightDriveMotor;
     DcMotor strafeDriveMotor;
     HardwareMap hwMap;
-    
+
     public HDrive(HardwareMap aHwMap){
         hwMap = aHwMap;
         DcMotor leftDriveMotor = hwMap.get(DcMotor.class, "leftDrive");
         DcMotor rightDriveMotor = hwMap.get(DcMotor.class, "rightDrive");
         DcMotor strafeDriveMotor = hwMap.get(DcMotor.class, "strafeDrive");
     }
-    
+
     public void driveForwardToPosition(){
+        DcMotor leftDriveMotor = hwMap.get(DcMotor.class, "leftDrive");
+        DcMotor rightDriveMotor = hwMap.get(DcMotor.class, "rightDrive");
+        DcMotor strafeDriveMotor = hwMap.get(DcMotor.class, "strafeDrive");
         leftDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         strafeDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        
+
         leftDriveMotor.setPower(0.75);
         rightDriveMotor.setPower(0.75);
         leftDriveMotor.setTargetPosition(100);
