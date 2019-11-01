feb6cd5050e97c68019ef0901fdaeaa3eb9f3e9d
IsaacIngram22 <iingram@students.casciahall.com>
2019-11-01 15:42:50 -0500

    Added new methods to HDrive

--- TestCode/HDrive.java
+++ TestCode/HDrive.java
@@ -12,23 +12,61 @@ public class HDrive {
     HardwareMap hwMap;
 
     public HDrive(HardwareMap aHwMap){
-        hwMap = aHwMap;
-        DcMotor leftDriveMotor = hwMap.get(DcMotor.class, "leftDrive");
-        DcMotor rightDriveMotor = hwMap.get(DcMotor.class, "rightDrive");
-        DcMotor strafeDriveMotor = hwMap.get(DcMotor.class, "strafeDrive");
+        this.hwMap = aHwMap;
+        this.leftDriveMotor = this.hwMap.get(DcMotor.class, "leftDrive");
+        this.rightDriveMotor = this.hwMap.get(DcMotor.class, "rightDrive");
+        this.strafeDriveMotor = this.hwMap.get(DcMotor.class, "strafeDrive");
     }
-
-    public void driveForwardToPosition(){
-        DcMotor leftDriveMotor = hwMap.get(DcMotor.class, "leftDrive");
-        DcMotor rightDriveMotor = hwMap.get(DcMotor.class, "rightDrive");
-        DcMotor strafeDriveMotor = hwMap.get(DcMotor.class, "strafeDrive");
-        leftDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        strafeDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftDriveMotor.setPower(0.75);
-        rightDriveMotor.setPower(0.75);
-        leftDriveMotor.setTargetPosition(100);
-        rightDriveMotor.setTargetPosition(100);
+    
+    public boolean leftDriveMotorBusy(){
+        return this.leftDriveMotor.isBusy();
     }
+    
+    public boolean rightDriveMotorBusy(){
+        return this.rightDriveMotor.isBusy();
+    }
+    
+    public boolean strafeDriveMotorBusy(){
+        return this.strafeDriveMotor.isBusy();
+    }
+    
+    public void resetEncoders(){
+        this.leftDriveMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.rightDriveMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.strafeDriveMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    
+    public void strafeToBuildingZone(){
+        this.strafeDriveMotor.setTargetPosition(6500);
+        this.strafeDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.strafeDriveMotor.setPower(0.75);
+    }
+    
+    public void driveAwayFromWall(){
+        this.leftDriveMotor.setTargetPosition(-3500);
+        this.rightDriveMotor.setTargetPosition(3500);
+        this.leftDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.rightDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.leftDriveMotor.setPower(0.75);
+        this.rightDriveMotor.setPower(0.75);
+    }
+    
+    public void stopMotors() {
+        // this.leftDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        // this.rightDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.leftDriveMotor.setPower(0);
+        this.rightDriveMotor.setPower(0);
+        this.strafeDriveMotor.setPower(0);
+    }
+    
+    public void driveForward(double power) {
+        this.leftDriveMotor.setPower(-power);
+        this.rightDriveMotor.setPower(power);
+    }
+    public void controlDrive(double leftPower, double rightPower, double strafePower) {
+        this.leftDriveMotor.setPower(leftPower);
+        this.rightDriveMotor.setPower(rightPower);
+        this.strafeDriveMotor.setPower(strafePower);
+    }
+    
 }
