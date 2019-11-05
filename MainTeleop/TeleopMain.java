a1d1170ddb359454c3a7746fb1a8171e2f3566d7
IsaacIngram22 <iingram@students.casciahall.com>
2019-11-05 17:56:25 -0600

    Created vision object to get vision values

--- MainTeleop/TeleopMain.java
+++ MainTeleop/TeleopMain.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.TestCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.TouchSensor;
@@ -10,10 +11,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.teamcode.APIs.FTCOmniDriveAPI;
+import org.firstinspires.ftc.teamcode.APIs.VisionTrackingAPI;
 
 @TeleOp(name="TeleopMain")
 
 public class TeleopMain extends LinearOpMode {
+
+VisionTrackingAPI vision = new VisionTrackingAPI();
 
     @Override
     public void runOpMode() {
@@ -29,14 +33,23 @@ public class TeleopMain extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
+        
+        vision.initialize(hardwareMap);
 
         while (opModeIsActive()) {
             RIPSteve.driveOmni(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
             
-            telemetry.addData("Intake button pressed" , intakeButton.isPressed());
-            telemetry.addData("Color sensor red value" , colorSensor.red());
-            telemetry.addData("Color sensor green value" , colorSensor.green());
-            telemetry.addData("Color sensor blue value" , colorSensor.blue());
+            // telemetry.addData("Intake button pressed" , intakeButton.isPressed());
+            // telemetry.addData("Color sensor red value" , colorSensor.red());
+            // telemetry.addData("Color sensor green value" , colorSensor.green());
+            // telemetry.addData("Color sensor blue value" , colorSensor.blue());
+            vision.searchForTargets();
+            // telemetry.addData("Robot X", vision.getX());
+            // telemetry.addData("Robot Y", vision.getY());
+            // telemetry.addData("Robot Z", vision.getZ());
+            // telemetry.addData("Robot Roll", vision.getRoll());
+            // telemetry.addData("Robot Pitch", vision.getPitch());
+            // telemetry.addData("Robot Heading", vision.getHeading());
             
             telemetry.update();
             
