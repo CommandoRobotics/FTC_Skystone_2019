d00cac57dbcb7a1cbb68f4dba44b466f1669d79d
IsaacIngram22 <iingram@students.casciahall.com>
2019-11-22 16:40:49 -0600

    Added control scheme to BishopKelleyQualifier

--- MainTeleop/BishopKelleyQualifer.java
+++ MainTeleop/BishopKelleyQualifer.java
@@ -7,6 +7,7 @@ import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import java.util.ArrayList;
 import java.util.List;
 import org.firstinspires.ftc.teamcode.APIs.FTCOmniDriveAPI;
+import org.firstinspires.ftc.teamcode.APIs.IntakeAPI;
 
 @TeleOp(name="BishopKelleyQualifier")
 
@@ -16,6 +17,7 @@ public class BishopKelleyQualifier extends LinearOpMode {
     public void runOpMode() {
 
         FTCOmniDriveAPI robot = new FTCOmniDriveAPI(hardwareMap);
+        IntakeAPI intake = new IntakeAPI(hardwareMap, telemetry);
 
         gamepad1.setJoystickDeadzone(0);
 
@@ -26,6 +28,23 @@ public class BishopKelleyQualifier extends LinearOpMode {
 
         while (opModeIsActive()) {
             robot.driveOmniJoystick(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+            //Elevator control logc
+            if(gamepad1.left_bumper && !gamepad1.right_bumper){
+              intake.controlElevator(-0.5);
+            } else if(gamepad1.right_bumper && !gamepad1.left_bumper){
+              intake.controlElevator(0.5);
+            } else {
+              intake.controlElevator(0);
+            }
+
+            if(gamepad1.left_trigger > 0.1 && !(gamepad1.right_trigger > 0.1)){
+              intake.controlIntake(gamepad1.left_trigger);
+            } else if(gamepad1.right_trigger > 0.1 && !(gamepad1.left_trigger > 0.1)){
+              intake.controlIntake(gamepad1.right_trigger);
+            } else {
+              intake.controlIntake(0);
+            }
         }
     }
 }
