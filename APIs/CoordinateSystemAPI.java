fa8803066110baeb8d113c95ca6e193cf2e181d7
IsaacIngram22 <iingram@students.casciahall.com>
2019-11-22 02:14:23 -0600

    Removed broken angle logic and left (hopefully) working simpler logic

--- APIs/CoordinateSystemAPI.java
+++ APIs/CoordinateSystemAPI.java
@@ -15,45 +15,32 @@ public class CoordinateSystemAPI {
     }
 
     public void calculateCoordinates(double currentX, double currentY, double currentHeading, double targetX, double targetY, double targetHeading){
-
-        private static final double X_AXIS_SCALE = 6000;
-        private static final double Y_AXIS_SCALE = 1000;
         double xAxisDistance = targetX-currentX;
         double yAxisDistance = targetY-currentY;
 
         double xAxisMultipliedInput = xAxisDistance;
         double yAxisMultipliedInput = yAxisDistance;
 
-        double goldenRatio;
-        double newCurrentHeading;
-
-        //Calculate the radius of the circle using the distance formula
-        double radiusOfCircle = Math.sqrt(Math.pow(xAxisMultipliedInput-targetX, 2)+Math.pow(yAxisMultipliedInput-targetY, 2));
-
-        //Flip the heading 180 degrees
-        if(currentHeading+180 > 360){
-            newCurrentHeading = currentHeading-180;
+        if(xAxisMultipliedInput > 100){
+            xAxisMultipliedInput = 1;
+        } else if(xAxisMultipliedInput < -100){
+            xAxisMultipliedInput = -1;
         } else {
-            newCurrentHeading = currentHeading+180;
+            xAxisMultipliedInput = 0.01*xAxisMultipliedInput;
         }
 
-        //Find the x and y value of the target point after it has been moved to the correct angle
-        double xAxisControllerInput = radiusOfCircle*(Math.toDegrees(Math.cos(Math.toRadians(newCurrentHeading))));
-        double yAxisControllerInput = radiusOfCircle*(Math.toDegrees(Math.sin(Math.toRadians(newCurrentHeading))));
-
-        if(xAxisControllerInput > yAxisControllerInput){
-          goldenRatio = yAxisControllerInput/xAxisControllerInput;
-          xAxisControllerInput = xAxisControllerInput/X_AXIS_SCALE;
-          yAxisControllerInput = yAxisControllerInput*goldenRatio;
-        } else if(yAxisControllerInput > xAxisControllerInput){
-          goldenRatio = xAxisControllerInput/yAxisControllerInput;
-          yAxisControllerInput = yAxisControllerInput/Y_AXIS_SCALE;
-          xAxisControllerInput = xAxisControllerInput*goldenRatio;
+        if(yAxisMultipliedInput > 100){
+            yAxisMultipliedInput = 1;
+        } else if(yAxisMultipliedInput < -100){
+            yAxisMultipliedInput = -1;
+        } else {
+            yAxisMultipliedInput = 0.01*yAxisMultipliedInput;
         }
 
-        leftMotorPower = yAxisControllerInput;
-        rightMotorPower = yAxisControllerInput;
-        strafeMotorPower = xAxisControllerInput;
+        leftMotorPower = yAxisMultipliedInput;
+        rightMotorPower = yAxisMultipliedInput;
+        strafeMotorPower = -xAxisMultipliedInput;
+
     }
 
     public double coordinatesLeftMotorPower(){
