6f80698a9dcb33025b0670620401d37e38613020
IsaacIngram22 <iingram@students.casciahall.com>
2019-11-29 13:08:32 -0600

    Added new autonomous code

--- MainAutonomous/SkystoneAutonomousMain.java
+++ MainAutonomous/SkystoneAutonomousMain.java
@@ -1,22 +1,4 @@
-/*
-Copyright 2018 FIRST Tech Challenge Team 9898a
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute,
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial
-portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-//package org.firstinspires.ftc.roverruckus.teamcode.autonomous;
-
-// import org.firstinspires.ftc.roverruckus.teamcode.apis.*;
-// import com.qualcomm.robotcore.hardware.Gyroscope;
+//This program assumes you are starting on the building side from the red alliance side
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -27,22 +9,57 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.APIs.*;
 
-@Autonomous
+@Autonomous(name="Test Autonomous")
 
 public class SkystoneAutonomousMain extends LinearOpMode {
-
-    //private static final int SAMPLE_TIME_ADDITION = 4000;
-    //private static final int SAMPLE_START_DELAY = 1000;
 
     @Override
     public void runOpMode() {
 
       FTCOmniDriveAPI chassis = new FTCOmniDriveAPI(hardwareMap, telemetry);
+      IntakeAPI intake = new IntakeAPI(hardwareMap, telemetry);
+      ColorSensorAPI color = new ColorSensorAPI(hardwareMap, "frontRightColor");
       chassis.resetEncoders();
       waitForStart();
 
-      chassis.driveStraightEnc(24,0.5f);
-      chassis.driveStrafeEnc(24,.4f);
+      //Move elevator up
+      intake.setHeight(0.5, 2);
 
+      //Drive from wall to front of platform
+      chassis.driveStraightEnc(30,0.5f);
+
+      //Drive sideway to platform
+      chassis.driveStrafeEnc(10,0.5f);
+
+      //Place block
+      intake.intakeOut();
+
+      //Wait 1 second
+      sleep(1000);
+
+      //Stop intake
+      intake.stopIntake();
+
+      //DROP GRABBER
+
+      //Move platform
+      chassis.driveStraightEnc(-27,0.5f);
+
+      //LIFT GRABBER
+
+      //Strafe left
+      chassis.driveStrafeEnc(-58,0.75f);
+
+      //Raise elevator above blocks
+      intake.setHeight(0.5, 5);
+
+      //Drive towards quarry
+      chassis.driveStraightEnc(30,0.5f);
+
+      //Drive left while scanning for skystone
+      while(color.isSkystoneDetected() == false){
+        chassis.controlChassis(0,0,.4);
+      }
+      chassis.controlChassis(0,0,0);
     }
 }
