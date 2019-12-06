5ad1725ba57cf778aa85234cecfa142f27883279
IsaacIngram22 <iingram@students.casciahall.com>
2019-12-06 01:27:40 -0600

    Add driveslowmode and capstoneplacer code to TeleopMain

--- MainTeleop/TeleopMain.java
+++ MainTeleop/TeleopMain.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.TestCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -18,11 +19,14 @@ public class TeleopMain extends LinearOpMode {
 
         FTCOmniDriveAPI robot = new FTCOmniDriveAPI(hardwareMap, telemetry);
         IntakeAPI intake = new IntakeAPI(hardwareMap, telemetry);
+        Servo capstonePlacer = hardwareMap.get(Servo.class, "capstonePlacer");
 
-        boolean aToggle = false;
         boolean previousA = false;
+        boolean slowModeActive = false;
 
         gamepad1.setJoystickDeadzone(0);
+
+        capstonePlacer.setPosition(0);
 
         telemetry.addLine("Robot Status : Initialized");
         telemetry.update();
@@ -30,35 +34,57 @@ public class TeleopMain extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            robot.driveOmniJoystick(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
-            if(gamepad1.left_trigger > 0 && gamepad1.right_trigger == 0){
-              intake.controlElevator(gamepad1.left_trigger);
-            } else if(gamepad1.right_trigger > 0 && gamepad1.left_trigger == 0){
-              intake.controlElevator(-gamepad1.right_trigger);
+
+            telemetry.addData("", slowModeActive);
+            telemetry.update();
+
+            if(gamepad1.a && gamepad1.a != previousA){
+              slowModeActive = !slowModeActive;
+            }
+            previousA = gamepad1.a;
+            if(!slowModeActive){
+              robot.driveOmniJoystick(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+              if(gamepad1.left_trigger > 0 && gamepad1.right_trigger == 0){
+                intake.controlElevator(gamepad1.left_trigger);
+              } else if(gamepad1.right_trigger > 0 && gamepad1.left_trigger == 0){
+                intake.controlElevator(-gamepad1.right_trigger);
+              } else {
+                intake.controlElevator(0);
+              }
+
+              if(gamepad1.left_bumper && !gamepad1.right_bumper){
+                intake.controlIntake(1);
+              } else if(gamepad1.right_bumper && !gamepad1.left_bumper){
+                intake.controlIntake(-1);
+              } else {
+                intake.controlIntake(0);
+              }
             } else {
-              intake.controlElevator(0);
+              robot.driveOmniJoystick(gamepad1.left_stick_x*0.5f, gamepad1.left_stick_y*0.5f, gamepad1.right_stick_x*0.5f);
+              if(gamepad1.left_trigger > 0 && gamepad1.right_trigger == 0){
+                intake.controlElevator(gamepad1.left_trigger);
+              } else if(gamepad1.right_trigger > 0 && gamepad1.left_trigger == 0){
+                intake.controlElevator(-gamepad1.right_trigger);
+              } else {
+                intake.controlElevator(0);
+              }
+
+              if(gamepad1.left_bumper && !gamepad1.right_bumper){
+                intake.controlIntake(0.3);
+                robot.driveStraight(-0.5f);
+              } else if(gamepad1.right_bumper && !gamepad1.left_bumper){
+                intake.controlIntake(-1);
+              } else {
+                intake.controlIntake(0);
+              }
             }
 
-            // if (gamepad1.a != previousA) {
-            //   aToggle = !aToggle;
-            // }
-            // if (aToggle) {
-            //   boolean finished = intake.setHeight(.4,5);
-            //   if (!finished) {
-            //     finished = intake.setHeight(.4,5);
-            //   } else {
-            //     intake.stopIntake();
-            //   }
-            // }
-            // previousA = gamepad1.a;
-
-            if(gamepad1.left_bumper && !gamepad1.right_bumper){
-              intake.controlIntake(1);
-            } else if(gamepad1.right_bumper && !gamepad1.left_bumper){
-              intake.controlIntake(-1);
+            if(gamepad1.b && gamepad1.dpad_left){
+              capstonePlacer.setPosition(90);
             } else {
-              intake.controlIntake(0);
+              capstonePlacer.setPosition(0);
             }
+
         }
     }
 }
